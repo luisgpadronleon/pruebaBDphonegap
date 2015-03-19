77613d73bf8a9f117620b7b43dc6cdefa5ab61f5
Luis Padrón <luisgpadronleon@gmail.com>
2015-03-19 12:05:31 -0430

    otra vez

--- conecta.php
+++ conecta.php
@@ -3,8 +3,8 @@
 //Conexión a base de datos
 $server = "localhost";
 $username = "root";
-$password = "Sofia1340";
-$database = "phonegap";
+$password = "ifNa67i8C4";
+$database = "puertola_phonegap";
 $con = mysql_connect($server, $username, $password) or die ("Error al conectar: " . mysql_error());
 mysql_select_db($database, $con);
 
