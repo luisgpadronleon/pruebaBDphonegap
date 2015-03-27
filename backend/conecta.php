880ac1fa452edf43a780033c48e28701cb4b27ef
Luis Padrón <luisgpadronleon@gmail.com>
2015-03-26 20:13:26 -0430

    mostrar datos

--- backend/conecta.php
+++ backend/conecta.php
@@ -3,7 +3,7 @@
 //Conexion a base de datos
 $server = "localhost";
 $username = "root";
-$password = "123654";
+$password = "Sofia1340";
 $database = "puertola_phonegap";
 $con = mysql_connect($server, $username, $password) or die ("Error al conectar: " . mysql_error());
 mysql_select_db($database, $con);
